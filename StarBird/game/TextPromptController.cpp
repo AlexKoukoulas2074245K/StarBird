c4f340cf3409612912347ba256bda5c954ffd4c4
Alex Koukoulas <alexios.koukoulas.2013@gmail.com>
2023-04-01 19:14:02 +0100

    TextPromptController clean up

--- StarBird/game/TextPromptController.cpp
+++ StarBird/game/TextPromptController.cpp
@@ -14,6 +14,15 @@
 
 ///------------------------------------------------------------------------------------------------
 
+static const float GLYPH_SCALE_INFERENCE_MAGIC = 1428.5714f;
+static const float LEFT_BOUND_MAGIC = 0.43f;
+static const float RIGHT_BOUND_MAGIC = 0.41f;
+static const float TEXT_WRAP_Y_OFFSET_MAGIC = 100.0f;
+static const float INIT_Y_OFFSET_MAGIC = 0.25f;
+static const float FADE_IN_DELAY_MULTIPLIER_MILLIS = 20.0f;
+
+///------------------------------------------------------------------------------------------------
+
 static std::unordered_map<char, Glyph>::const_iterator GetGlyphIter(char c, const FontDefinition& fontDef)
 {
     auto findIter = fontDef.mGlyphs.find(c);
@@ -46,16 +55,15 @@ TextPromptController::TextPromptController(Scene& scene, std::unique_ptr<BaseAni
     {
         const auto& font = fontOpt->get();
         
-        const float glyphScale = ((scale.x + scale.y)/2.0f)/1428.5714f;
-        const float leftBoundMagic = 0.43f;
-        const float rightBoundMagic = 0.41f;
-        const float yOffsetMagic = 0.25f;
-        const float wrapYMagic = 100.0f;
+        // Define constants
+        const float glyphScale = ((scale.x + scale.y)/2.0f)/GLYPH_SCALE_INFERENCE_MAGIC;
+        const float xCutoff = position.x + scale.x * RIGHT_BOUND_MAGIC;
         
-        float xCursor = position.x - scale.x * leftBoundMagic;
-        float xCutoff = position.x + scale.x * rightBoundMagic;
-        float yCursor = position.y + scale.y * yOffsetMagic;
+        // and initial positions
+        float xCursor = position.x - scale.x * LEFT_BOUND_MAGIC;
+        float yCursor = position.y + scale.y * INIT_Y_OFFSET_MAGIC;
         
+        // Split text to words and re-add space to the end of each word
         auto words = strutils::StringSplit(text, ' ');
         for (size_t i = 0; i < words.size() - 1; ++i)
         {
@@ -76,12 +84,13 @@ TextPromptController::TextPromptController(Scene& scene, std::unique_ptr<BaseAni
                 cursorCopy += (glyph.mWidthPixels * glyphScale) * 0.5f + (nextGlyph.mWidthPixels * glyphScale) * 0.5f;
                 if (cursorCopy > xCutoff)
                 {
-                    xCursor = position.x - scale.x * leftBoundMagic;
-                    yCursor -= glyphScale * wrapYMagic;
+                    xCursor = position.x - scale.x * LEFT_BOUND_MAGIC;
+                    yCursor -= glyphScale * TEXT_WRAP_Y_OFFSET_MAGIC;
                     break;
                 }
             }
             
+            // Create a scene object for each char in the text prompt
             for (size_t j = 0; j < currentWord.size(); ++j)
             {
                 const auto& glyph = GetGlyphIter(currentWord[j], font)->second;
@@ -90,13 +99,13 @@ TextPromptController::TextPromptController(Scene& scene, std::unique_ptr<BaseAni
                 SceneObject glyphSO;
                 glyphSO.mPosition = glm::vec3(xCursor, yCursor, position.z + 0.5f);
                 glyphSO.mScale = glm::vec3(glyphScale, glyphScale, position.z + 0.5f);
-                glyphSO.mAnimation = std::make_unique<SingleFrameAnimation>(FontRepository::GetInstance().GetFont(game_constants::DEFAULT_FONT_NAME)->get().mFontTextureResourceId, resService.LoadResource(resources::ResourceLoadingService::RES_MESHES_ROOT + game_constants::QUAD_MESH_FILE_NAME), resService.LoadResource(resources::ResourceLoadingService::RES_SHADERS_ROOT + game_constants::CUSTOM_ALPHA_SHADER_FILE_NAME), glm::vec3(1.0f), false);
+                glyphSO.mAnimation = std::make_unique<SingleFrameAnimation>(FontRepository::GetInstance().GetFont(game_constants::DEFAULT_FONT_NAME)->get().mFontTextureResourceId, resService.LoadResource(resources::ResourceLoadingService::RES_MESHES_ROOT + game_constants::QUAD_MESH_FILE_NAME), resService.LoadResource(resources::ResourceLoadingService::RES_SHADERS_ROOT + game_constants::CUSTOM_ALPHA_SHADER_FILE_NAME), glm::vec3(glyphScale), false);
                 glyphSO.mShaderFloatUniformValues[game_constants::CUSTOM_ALPHA_UNIFORM_NAME] = fadeIn ? 0.0f : 1.0f;
                 glyphSO.mFontName = game_constants::DEFAULT_FONT_NAME;
                 glyphSO.mSceneObjectType = SceneObjectType::WorldGameObject;
                 glyphSO.mName = strutils::StringId(std::to_string(charCounter));
                 glyphSO.mText = std::string(1, currentWord[j]);
-                mSceneObjectNamesToTransparencyDelayMillis[glyphSO.mName] = charCounter * 20.0f;
+                mSceneObjectNamesToTransparencyDelayMillis[glyphSO.mName] = charCounter * FADE_IN_DELAY_MULTIPLIER_MILLIS;
                 mScene.AddSceneObject(std::move(glyphSO));
                 
                 if (i != words.size() - 1 || j != currentWord.size() - 1)
